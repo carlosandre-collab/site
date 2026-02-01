c91b2dd6fb788eb06ab88c1dbe0c96040dc9b349
carlosandre-collab <carlos.andre@alavanka.com>
2026-02-01 19:46:57 -0300

    Update nav para incluir o home e o menu do index

--- public/components/nav.js
+++ public/components/nav.js
@@ -73,6 +73,7 @@
         + '    <span></span><span></span><span></span>'
         + '  </button>'
         + '  <div class="nav-links" id="navLinks">'
+        + '    <a href="' + (isIndex ? '#' : indexUrl) + '" title="Home"><svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg></a>'
         + '    <a href="' + sectionLink('problema') + '" data-i18n="nav.problem">O Problema</a>'
         + '    <a href="' + sectionLink('solucao') + '" data-i18n="nav.solution">A Solução</a>'
         + '    <a href="' + sectionLink('porque') + '" data-i18n="nav.why">Por Que Nós</a>'
